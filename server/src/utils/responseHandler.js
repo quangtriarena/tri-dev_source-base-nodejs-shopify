0118c29c7ff7762fe6ec22b0e62dc70ea7316bbb
tri.luu@arenacommerce.com <tri.luu@arenacommerce.com>
2024-06-11 17:58:52 +0700

    update structure code

--- server/src/utils/responseHandler.js
+++ server/src/utils/responseHandler.js
@@ -1,5 +1,5 @@
 const ResponseHandler = {
-	success: ({ res, data, statusCode = 2000, message = "" }) =>
+	success: ({ res, data, statusCode = 200, message = "" }) =>
 		res.status(statusCode).json({ status: "success", message, data }),
 	error: ({ res, message, statusCode = 500 }) =>
 		res.status(statusCode).json({ status: "error", message }),
