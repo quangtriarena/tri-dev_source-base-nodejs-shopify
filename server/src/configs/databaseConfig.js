b358c204c9285693c036114a2b9007274b5725f7
tri.luu@arenacommerce.com <tri.luu@arenacommerce.com>
2024-06-18 13:45:03 +0700

    update database config

--- server/src/configs/databaseConfig.js
+++ server/src/configs/databaseConfig.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv'
-dotenv.config()
+const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development'
+dotenv.config({ path: envFile })
+
 import { Sequelize } from 'sequelize'
 
 class Database {
