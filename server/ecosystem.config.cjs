6722b1d8bbb868c91b3061c08f42b93e47414e6c
tri.luu@arenacommerce.com <tri.luu@arenacommerce.com>
2024-06-19 01:55:54 +0700

    update ecosystem

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -4,6 +4,8 @@ module.exports = {
             name: 'node-test',
             script: './src/index.js',
             watch: true,
+            instances: 'max',
+            exec_mode: 'cluster',
             ignore_watch: ['node_modules'],
             env: {
                 NODE_ENV: 'development',
@@ -22,7 +24,7 @@ module.exports = {
             repo: 'GIT_REPOSITORY',
             path: 'DESTINATION_PATH',
             'pre-deploy-local': '',
-            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.cjs --env production',
             'pre-setup': '',
         },
     },
